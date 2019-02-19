2c1e6b555ad9a0e3a2f263825d2c35a410612804
joseLagrosa <37275113+joseLagrosa@users.noreply.github.com>
2019-02-19 16:43:05 -0600

    Add files via upload

--- Webserver_Testing/tempSQL.py
+++ Webserver_Testing/tempSQL.py
@@ -3,10 +3,10 @@ import time
 import os
 import datetime
 import requests
-import urllib
+import urllib2
 from time import strftime
 import paho.mqtt.client as mqtt
-
+    
     # Callback fires when conected to MQTT broker.
 def on_connect(client, userdata, flags, rc):
     print('Connected with result code {0}'.format(rc))
@@ -19,7 +19,7 @@ def on_message(client, userdata, msg):
     # Decode temperature and humidity values from binary message paylod.
     T,RH = [float(x) for x in msg.payload.decode("utf-8").split(',')]
     dt = datetime.datetime.now().strftime('%Y-%m-%d %H"%M:%S')
-    urllib.urlopen("http://www.m0nitorsystem.com/add_data.php?dt="+dt+"&T="+T+"&RH="+RH).read()
+    urllib2.urlopen("https://www.m0nitorsystem.com/add_data.php?dt="+dt+"&T="+T+"&RH="+RH).read()
     print (dt)
     print('{0}°C {1}%'.format(T, RH))
     
