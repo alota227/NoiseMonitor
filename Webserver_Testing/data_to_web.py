572827d403325b0c31b4387cb230f0947acacf06
joseLagrosa <37275113+joseLagrosa@users.noreply.github.com>
2019-03-29 10:15:27 -0600

    Add files via upload

--- Webserver_Testing/data_to_web.py
+++ Webserver_Testing/data_to_web.py
@@ -5,10 +5,73 @@ import datetime
 import urllib.request
 import urllib.parse
 from time import strftime
-#from dateutil.tz import gettz
+
 
 firebase = firebase.FirebaseApplication('https://noise-c2b8e.firebaseio.com/', None)
 
+def direction (north1,east1,south1,west1): #compares the dB levels of each microphone and deterimine the direction of the sound
+    
+    dire = ""
+    addNE= (north1 + east1)
+    addNW= (north1 + west1)
+    addSE= (south1 + east1)
+    addSW= (south1 + west1)
+    subNE= (north1 - east1)
+    subNW= (north1 - west1)
+    subSE= (south1 - east1)
+    subSW= (south1 - west1)    
+    
+    ## Checks for N, NE, NW
+    if (north1>east1 and north1>south1 and north1>west1): ## checks if north is largest reading
+        if ((addNE > addNW) and (addNE > addSW) and (addNE > addSE ) and (subNE > -6 and subNE < 6)):
+            print ("North East")
+            dire = "North East"
+        elif ((addNW > addNE) and (addNW > addSW) and (addNW > addSE) and (subNW > -6 and subNW < 6)):
+            print ("North West")
+            dire = "North West"
+        else:
+            print ("North")
+            dire = "North"
+            
+     ## Checks for S, SE, SW
+    if (south1>east1 and south1>north1 and south1>west1): ## checks if south is largest reading
+        if ((addSE > addNW) and (addSE > addSE) and (addSE > addSE) and (subSE > -6 and subSE < 6)):
+            print ("South East")
+            dire = "South East"
+        elif ((addSW > addNE) and (addSW > addSW) and (addSW > addSE) and (subSW > -6 and subSW < 6)):
+            print ("South West")
+            dire = "South West"
+        else:
+            print ("South")
+            dire = "South"
+            
+     ## Checks for E, NE, SE
+    if (east1>south1 and east1>north1 and east1>west1): ## checks if east is largest reading
+        if ((addSE > addNW) and (addSE > addSW) and (addSE > addNE) and (subSE > -6 and subSE < 6)):
+            print ("South East")
+            dire = "South East"
+        elif ((addNE > addNW) and (addNE > addSW) and (addNE > addSE) and (subNE > -6 and subNE < 6)):
+            print ("North East")
+        else:
+            print ("East")
+            dire = "East"
+
+    # Checks for W, NW, SW
+   if (west1>south1 and west1>north1 and west1>east1): ## checks if west is largest reading
+       if ((addSW > addNW) and (addSW > addSE) and (addSW > addNE) and (subSW > -6 and subSW < 6)):
+           print ("South West")
+           dire = "South West"
+        
+       elif ((addNW > addNE) and (addNW > addSW) and (addNW > addSE) and (subNW > -6 and subNW < 6) ): 
+           print ("North West")
+           dire = "Norh West"
+           return dire
+       else:
+           print ("West")
+           dire = "West"
+
+    return dire
+           
 def average(n,e,s,w):
     return ((int(n)+int(e)+int(s)+int(w))/4.0)
     
@@ -27,21 +90,25 @@ while True:
     
     east=firebase.get('/East',None)
     print ('MIC2: '+ str(east))
-#    
+ 
     south=firebase.get('/South',None)
     print ('MIC3: '+ str(south))
     
     west=firebase.get('/West',None)
     print ('MIC4: '+ str(west))
-#    
-    ave = average(oneNorth,east,south,west)
+    
+    ave = average(int(oneNorth),int(east),int(south),int(west))
     avg = str(ave)
     print(avg)
+    
+    direct = direction(int(oneNorth),int(east),int(south),int(west))
+    print(direct)
     
     dt = datetime.datetime.now().strftime('%Y-%m-%d %H"%M:%S')
     #dt.datetime.now(gettz("Europe/Madrid")).isoformat()
     print (dt)
     
+    time.sleep(0.5)
     temp = "%.1f" %temperature
     hum = "%.1f" %humidity
     
@@ -54,6 +121,7 @@ while True:
     data['oneSouth'] = south
     data['oneWest'] = west
     data['spl'] = avg
+    data['dir'] = direct
     
     url_values = urllib.parse.urlencode(data)
     print(url_values)
@@ -62,6 +130,5 @@ while True:
     data = urllib.request.urlopen(full_url)
     print ('------------')
     
-    
-    
-    time.sleep(2)
+
+       
